579b94ab5247424bd3a2b4a4a46c546da062c6d3
Filipe Salviano <filipesalviano@proton.me>
2025-08-01 19:21:32 -0300

    command delete fixed

--- chiaki-bot/src/commands/delete.ts
+++ chiaki-bot/src/commands/delete.ts
@@ -10,22 +10,42 @@ const deleteMessage: IChiakiCommand = {
   },
 
   async execute(client, flag, arg, M) {
-    if (!M.quoted?.message?.extendedTextMessage?.contextInfo) {
+    if (!M.quoted) {
       await M.reply("🟥 *Responda a mensagem que você quer apagar!*");
       return;
     }
 
-    const context = M.quoted.message.extendedTextMessage.contextInfo;
+    const isGroup = M.isGroup;
+    const key = M.quoted.key;
+
+    if (isGroup) {
+      const metadata = await client.groupMetadata(M.from);
+      const botNumber = client.user.id.split(":")[0] + "@s.whatsapp.net";
+      const sender = M.sender;
+      const botInfo = metadata.participants.find((x) => x.id === botNumber);
+      const senderInfo = metadata.participants.find((x) => x.id === sender);
+      const isBotAdmin = botInfo?.admin === "admin";
+      const isSenderAdmin = senderInfo?.admin === "admin" || senderInfo?.admin === "superadmin";
+
+      if (!isBotAdmin) {
+        await M.reply(`🟥 *Não posso apagar mensagens, não sou admin nesse grupo.*`);
+        return;
+      }
+
+      if (!isSenderAdmin) {
+        await M.reply(`🟥 *Apenas administradores podem executar este comando!*`);
+        return;
+      }
+    }
 
     await client.sendMessage(M.from, {
       delete: {
         remoteJid: M.from,
-        fromMe: false,
-        id: context.stanzaId!,
-        participant: context.participant!,
+        fromMe: key.fromMe!,
+        id: key.id!,
+        participant: M.quoted.participant,
       },
     });
-  },
-};
+  },};
 
 export default deleteMessage;

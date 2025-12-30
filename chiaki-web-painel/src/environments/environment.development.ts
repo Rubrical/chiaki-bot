5d315eb33533c019a2479385fd3c4884fa92c7a4
Filipe Salviano <filipesalviano@proton.me>
2025-12-30 20:24:16 -0300

    Proxy reverso configurado corretamente

--- chiaki-web-painel/src/environments/environment.development.ts
+++ chiaki-web-painel/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: `${window.origin}:3000/api`,
-  socketUrl: `${window.origin}:3001`,
-  botServerUrl: `${window.origin}:3002`,
+  backendUrl: `/api`,
+  socketUrl: `/socket`,
+  botServerUrl: `/bot`,
 };
--- chiaki-web-painel/src/environments/environment.ts
+++ chiaki-web-painel/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: `${window.origin}:3000/api`,
-  socketUrl: `${window.origin}:3001`,
-  botServerUrl: `${window.origin}:3002`,
+  backendUrl: `/api`,
+  socketUrl: `/socket`,
+  botServerUrl: `/bot`,
 };
